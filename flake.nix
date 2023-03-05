fe6c884d4e7b39dc96ea89cf61448c1559e49c0b
Robin Appelman <robin@icewind.nl>
2023-03-05 22:21:59 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -2,21 +2,36 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/release-22.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    rust-overlay,
   }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = (import nixpkgs) {
         inherit system;
+        overlays = [rust-overlay.overlays.default];
       };
     in rec {
       # `nix develop`
       devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rustc cargo bacon cargo-edit cargo-outdated clippy cargo-audit cargo-msrv valgrind hyperfine];
+        nativeBuildInputs = with pkgs; [
+#          rust-bin.stable.latest.default
+          bacon
+          cargo-edit
+          cargo-outdated
+          clippy
+          cargo-audit
+          cargo-msrv
+          valgrind
+          hyperfine
+          cargo-expand
+          rustup
+        ];
       };
     });
 }
